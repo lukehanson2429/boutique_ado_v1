d8d076590769275260efb87add2075cd932b5ea0
lukehanson2429 <lukehanson2429@gmail.com>
2021-08-04 15:02:43 +0000

    no changes

--- bag/views.py
+++ bag/views.py
@@ -14,7 +14,13 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['size']
     bag = request.session.get('bag', {})
+
+    if size:
+        
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
